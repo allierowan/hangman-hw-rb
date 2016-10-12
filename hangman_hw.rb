87954b7b825ca9b617b06330efef4f7ca2f08a90
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-12 15:12:41 -0400

    created method to check if you won. Created some body code to run the game

--- hangman_hw.rb
+++ hangman_hw.rb
@@ -1,11 +1,4 @@
 # A version of hangman that's playable in the terminal
-
-# array of possible words that we can play with
-hangman_words = ["the iron yard"]
-game_mode = "easy"
-all_guesses = []
-#game_word = rand_item(hangman_words)
-
 # puts all characters (except the space) of a word into an array
 def mk_string_array(word)
   return (word.gsub(" ", "")).chars.to_a
@@ -21,11 +14,11 @@ end
 def how_many_guesses(word, mode = "easy")
   case mode
   when "easy"
-    num_guesses = unique_chars(word)+15
+    num_guesses = num_unique_chars(word)+15
   when "medium"
-    num_guesses = unique_chars(word)+9
+    num_guesses = num_unique_chars(word)+9
   when "hard"
-    num_guesses = unique_chars(word)+6
+    num_guesses = num_unique_chars(word)+6
   end
   return num_guesses
 end
@@ -36,26 +29,12 @@ def rand_item(array)
   return array[rand_index]
 end
 
-# output the game board
-def put_game_board(game_word)
-  word_string = ""
-  char_array = game_word.chars.to_a
-  char_array.each do |char|
-    if char == " "
-      word_string += "  "
-    else
-      word_string += "_ "
-    end
-  end
-  puts word_string
-end
 
-# update the game board based on a guess
+# output an appropriate gameboard based on what the user has guessed
 def update_game_board(game_word, guess_char_array)
   new_board = ""
   game_word_array = game_word.chars.to_a
   game_word_array.each do |char|
-    p char
     if guess_char_array.index(char.downcase)
       new_board += char + " "
     elsif char == " "
@@ -65,4 +44,28 @@ def update_game_board(game_word, guess_char_array)
     end
   end
   puts new_board
+end
+
+# check if the player won the game
+def did_you_win?(game_word, guess_char_array)
+  game_word_array = mk_string_array(game_word.downcase).uniq
+  return game_word_array&guess_char_array.uniq == game_word_array
+end
+
+# gets a guess from the user and adds it to the guess_array
+hangman_words = ["the iron yard"]
+game_mode = "easy"
+all_guesses = []
+game_word = rand_item(hangman_words)
+remaining_guesses = how_many_guesses(game_word, game_mode)
+puts "Welcome to Hangman! Please guess a valid character"
+update_game_board(game_word, [])
+won = false
+
+until remaining_guesses == 0 || won == true
+  guess_character = (gets.chomp).downcase
+  all_guesses << guess_character
+  update_game_board(game_word, all_guesses)
+  remaining_guesses -= 1
+  won = did_you_win?(game_word, all_guesses)
 end
