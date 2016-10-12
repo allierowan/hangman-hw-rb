5f05bd99e799fc91210953035d26f9ba065015de
Aileen Rowan <aileenrowan@Aileens-MacBook-Air.local>
2016-10-12 13:31:47 -0400

    fleshed out unique_chars and how_many_guesses methods

--- hangman_hw.rb
+++ hangman_hw.rb
@@ -2,14 +2,24 @@
 
 # array of possible words that we can play with
 hangman_words = ["the iron yard"]
+game_mode = "easy"
 
 # how many unique characters in a string
 def unique_chars(word)
-  char_array = word.chars.to_a.uniq
+  no_spaces = word.sub(" ", "")
+  char_array = no_spaces.downcase.chars.to_a.uniq
   char_array.length
 end
 
 # how many guesses will the user get, which is based on how many unique letters are in the word
-def num_guesses(word)
-  
+def how_many_guesses(word, mode = "easy")
+  case mode
+  when "easy"
+    num_guesses = unique_chars(word)+15
+  when "medium"
+    num_guesses = unique_chars(word)+9
+  when "hard"
+    num_guesses = unique_chars(word)+6
+  end
+  return num_guesses
 end
